ddde98ae006aef60c8ca45ea0680e34ad2e9f4dc
Félix-Antoine Côté <felix-antoine.cote.2@ulaval.ca>
2023-09-22 11:53:12 -0400

    histogrammes

--- Lab_1/Scripts/Construire_histogrammes.py
+++ Lab_1/Scripts/Construire_histogrammes.py
@@ -3,15 +3,15 @@ import lvm_read
 import matplotlib.pyplot as plt
 
 #On va chercher tous les noms fichiers de données à Scatter
-liste_fich = os.listdir("Lab_1/Données/Scatter")
+liste_fich = os.listdir("Lab_1/Données/Histogrammes")
 
 
 for fichier in liste_fich:
     #On ouvre chacun des fichiers
     print(fichier)
-    lvm = lvm_read.read(f"Lab_1/Données/Scatter/{fichier}", dump_file=False)
-    plt.plot(lvm[0]['data'], marker='.')
+    lvm = lvm_read.read(f"Lab_1/Données/Histogrammes/{fichier}", dump_file=False)
+    plt.hist(lvm[0]['data'])
     plt.ylabel("Potentiel [V]")
     plt.xlabel("Temps [1/10s]")
-    plt.savefig(f"Lab_1/Figures/Graphiques/{fichier}.png")
+    plt.savefig(f"Lab_1/Figures/Histogrammes/{fichier}.png")
     plt.clf()
