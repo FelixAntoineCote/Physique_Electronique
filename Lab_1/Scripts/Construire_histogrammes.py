d4df0b36e6c8d08b78b916da9b6e78cbb78f0c90
Félix-Antoine Côté <felix-antoine.cote.2@ulaval.ca>
2023-09-22 12:21:55 -0400

    histogrammes retravaillés

--- Lab_1/Scripts/Construire_histogrammes.py
+++ Lab_1/Scripts/Construire_histogrammes.py
@@ -1,17 +1,40 @@
 import os
 import lvm_read
 import matplotlib.pyplot as plt
+import numpy as np
 
-#On va chercher tous les noms fichiers de données à Scatter
+
+#Les histogrammes sans les résistances
 liste_fich = os.listdir("Lab_1/Données/Histogrammes")
-
-
+liste_fich.remove("Résistances")
 for fichier in liste_fich:
     #On ouvre chacun des fichiers
     print(fichier)
     lvm = lvm_read.read(f"Lab_1/Données/Histogrammes/{fichier}", dump_file=False)
+    
+    #On va chercher les informations de l'histogramme avec numpy
+    counts, bins = np.histogram(lvm[0]['data'])
+
+    #On plot les données
+    plt.stairs(counts, bins, fill=True)
+    plt.xlabel("Potentiel [V]")
+
+    #On enregistre les figures avant de les clear
+    plt.savefig(f"Lab_1/Figures/Histogrammes/{fichier}.png")
+    plt.clf()
+
+
+#Les histogrammes avec les résistances
+liste_fich = os.listdir("Lab_1/Données/Histogrammes/Résistances")
+for fichier in liste_fich:
+    #On ouvre chacun des fichiers
+    print(fichier)
+    lvm = lvm_read.read(f"Lab_1/Données/Histogrammes/Résistances/{fichier}", dump_file=False)
+
+    #On plot les données
     plt.hist(lvm[0]['data'])
-    plt.ylabel("Potentiel [V]")
-    plt.xlabel("Temps [1/10s]")
+    plt.xlabel("Potentiel [V]")
+
+    #On enregistre les figures avant de les clear
     plt.savefig(f"Lab_1/Figures/Histogrammes/{fichier}.png")
     plt.clf()
